a78d66bc87b29b33fcf8b21d25d44f89f7bc80d4
Liaana <47424159+Liaana@users.noreply.github.com>
2021-04-22 10:51:42 +0300

    Liana (#2)
    * Added server packet structures.
    
    * Changed movable object packet structure.

--- shared/packet.h
+++ shared/packet.h
@@ -19,13 +19,50 @@ struct PacketClientMessage {
 };
 
 /*Server Packets*/
+struct PacketServerIdentification {
+        unsigned char protoVersion;
+        unsigned int clientAccepted;
+};
+
+struct PacketGameAreaInfo {
+        unsigned char sizeX;
+        unsigned char sizeY;
+        unsigned char* blockIDs;
+};
+
+struct PacketMovableObjectInfo {
+        unsigned char objectType;
+        unsigned int objectID;
+        float objectX;
+        float objectY;
+        char movement;
+};
+
+struct PacketMovableObjects {
+        unsigned char objectCount;
+        struct PacketMovableObjectInfo *movableObjects; 
+};
+
+struct PacketServerMessage {
+        char messageType;
+        char message[256];
+};
+
+struct PacketPlayerInfo {
+        unsigned int playerCount;
+        unsigned int playrID;
+        char playerName[32];
+        char playerColor;
+        unsigned int playerPoints;
+        unsigned char playerLives;
+};
 
 /*Implementation*/
 struct PacketCallbacks {
-        void (*callback[256])(void *);
+        void (*callback[256])(void*);
 };
 
-int PacketEncode(char * buffer, unsigned char type, void * packet);
-void PacketDecode(char * buffer, int len, struct PacketCallbacks * callbacks);
+int PacketEncode(char* buffer, unsigned char type, void* packet);
+void PacketDecode(char* buffer, int len, struct PacketCallbacks* callbacks);
 
 #endif
